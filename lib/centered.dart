c3d14706b544692ddf7c5e17c356ac351abf1281
Mason Kubiak <mayakubiak10@gmail.com>
2025-02-06 11:39:41 -0500

    Finished gradient container and centered widget

---
+++ lib/centered.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Centered extends StatelessWidget {
+  Centered(this.text, {super.key});
+  String text;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        text,
+        style: const TextStyle(color: Colors.white),
+        textScaler: TextScaler.linear(1.5),
+      ),
+    );
+  }
+}
